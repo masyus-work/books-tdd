c6176123a960b607d3a97676b8e5aced16503299
hiroki.masui <hiroki.masui@yourmystar.jp>
2020-07-19 19:44:16 +0900

    第16章完了

--- src/Expression.php
+++ src/Expression.php
@@ -5,6 +5,8 @@ require_once('src/Bank.php');
 
 interface Expression
 {
+    public function times(int $multiplier): Expression;
+
     public function plus(Expression $addend): Expression;
 
     public function reduce(Bank $bank, string $to): Money;
